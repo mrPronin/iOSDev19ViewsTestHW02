a07ac0ea2cf9796a13cf0a30b29a3460840c4034
Alexander Pronin <pronin.alx@gmail.com>
2014-02-23 17:22:35 +0200

    First commit

--- 19ViewsTestHW02/RITViewController.m
+++ 19ViewsTestHW02/RITViewController.m
@@ -17,7 +17,57 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+    
+    CGRect rect;
+    UIView *view, *chessboard;
+    NSUInteger cellCount        = 8;
+    NSUInteger borderOffset     = 2;
+    NSUInteger minimumSize      = MIN(CGRectGetWidth(self.view.frame), CGRectGetHeight(self.view.frame));
+    NSUInteger maximumSize      = MAX(CGRectGetWidth(self.view.frame), CGRectGetHeight(self.view.frame));
+    NSUInteger y                = (maximumSize - minimumSize) / 2;
+    
+    // calculate cell size and field size
+    minimumSize-= borderOffset * 2;
+    NSUInteger cellSize         = minimumSize / cellCount;
+    NSUInteger fieldSize        = cellSize * cellCount + borderOffset * 2;
+    
+    // set border
+    rect                        = CGRectMake(1, y, fieldSize, fieldSize);
+    chessboard                  = [[UIView alloc] initWithFrame:rect];
+    chessboard.backgroundColor  = [UIColor blackColor];
+    [self.view addSubview:chessboard];
+    
+    rect                        = CGRectMake(
+                                             borderOffset / 2,
+                                             borderOffset / 2,
+                                             CGRectGetWidth(rect) - borderOffset,
+                                             CGRectGetHeight(rect) - borderOffset
+                                             );
+    
+    view                        = [[UIView alloc] initWithFrame:rect];
+    view.backgroundColor        = [UIColor whiteColor];
+    [chessboard addSubview:view];
+    
+    y= borderOffset;
+    
+    // draw cells
+    for (int i = 0; i < cellCount; i++) {
+        
+        NSUInteger x            = cellSize * (i % 2) + borderOffset;
+        
+        for (int j = 0; j < cellCount / 2; j++) {
+            
+            rect                    = CGRectMake(x, y, cellSize, cellSize);
+            view                    = [[UIView alloc] initWithFrame:rect];
+            view.backgroundColor    = [UIColor blackColor];
+            [chessboard addSubview:view];
+            
+            x+= cellSize * 2;
+        }
+        
+        y+= cellSize;
+    }
+    
 }
 
 - (void)didReceiveMemoryWarning
