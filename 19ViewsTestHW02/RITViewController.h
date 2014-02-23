5a6f5be09e8cf533124142628a6a827ace21dc45
Alexander Pronin <pronin.alx@gmail.com>
2014-02-23 17:20:32 +0200

    Initial Commit

---
+++ 19ViewsTestHW02/RITViewController.h
@@ -0,0 +1,13 @@
+//
+//  RITViewController.h
+//  19ViewsTestHW02
+//
+//  Created by Pronin Alexander on 23.02.14.
+//  Copyright (c) 2014 Pronin Alexander. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RITViewController : UIViewController
+
+@end
